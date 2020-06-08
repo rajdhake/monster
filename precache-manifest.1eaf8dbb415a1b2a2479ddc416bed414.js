74d518308c6eea9e80ca57d59eb07e03c3ca9f10
raj dhake <your.raj12345dhake@gmail.com>
2020-06-08 11:01:29 +0530

    Updates

---
+++ precache-manifest.1eaf8dbb415a1b2a2479ddc416bed414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857a940d7a4c0bcbc8cf129bbf885251",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "d869ffd8a2be8df37c0e",
+    "url": "/monster/static/css/main.a6e5c08b.chunk.css"
+  },
+  {
+    "revision": "04b4751df5e9d7891449",
+    "url": "/monster/static/js/2.e7c5a68a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.e7c5a68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d869ffd8a2be8df37c0e",
+    "url": "/monster/static/js/main.7270f986.chunk.js"
+  },
+  {
+    "revision": "29dbeaf9e3f5bfcbde75",
+    "url": "/monster/static/js/runtime-main.279536a6.js"
+  }
+]);
